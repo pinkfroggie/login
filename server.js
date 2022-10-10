581e7efd97502f008c1d47b3cbe92175f5c91e1b
ivy <aibiimaknae@gmail.com>
2022-10-10 15:38:05 -0500

    cors added

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ require('./config/db');
 const app = require('express')();
 const port = process.env.PORT || 3100;
 
+// cors
+const cors = require('cors');
+app.use(cors());
+
 const UserRouter = require('./api/User');
 
 // for accepting post form data
